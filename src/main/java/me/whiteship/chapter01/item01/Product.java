a06dabe67985a4e66754b1319f1bbae439096205
u-nij <u.nij0509@gmail.com>
2023-07-19 16:57:02 +0900

    아이템 1. 핵심 정리 - 정적 팩터리 메서드의 장점2

--- src/main/java/me/whiteship/chapter01/item01/Product.java
+++ src/main/java/me/whiteship/chapter01/item01/Product.java
@@ -6,13 +6,23 @@ import java.util.Set;
 public class Product {
 
     public static void main(String[] args) {
+//        Settings settings = new Settings(); // 불가능
+
         Settings settings1 = Settings.getInstance();
         Settings settings2 = Settings.getInstance();
 
         System.out.println(settings1);
         System.out.println(settings2);
+        // 동일
 
+        // Boolean 같은 경우에도, 미리 만들어둔 상수 TRUE, FALSE를 이용
+        // 매개변수에 따라 각기 다른 인스턴스를 리턴하는 기능 또한 정적 팩토리 메서드를 사용하면 가능하다.
         Boolean.valueOf(false);
         EnumSet.allOf(Difficulty.class);
+
+        /*
+        플라이웨이트 패턴(재사용 가능한 객체 인스턴스를 공유시켜 메모리 사용량을 최소화하는 구조 디자인 패턴)과 유사.
+        자주 사용하는 객체들을 미리 만들어놓고, 필요한 인스턴스를 꺼내 쓸 수 있게 하는 것에서 통용되는 개념이 있기 때문에 언급됨.
+        */
     }
 }
--- src/main/java/me/whiteship/chapter01/item01/Settings.java
+++ src/main/java/me/whiteship/chapter01/item01/Settings.java
@@ -12,10 +12,24 @@ public class Settings {
 
     private Difficulty difficulty;
 
+    /*
+    자바의 생성자는 호출할 때마다 새로운 인스턴스를 만든다.
+    경우에 따라 인스턴스를 매번 만들지, 특정한 경우에만 만들지 등 인스턴스를 만드는 방법을 통제해야 할 필요가 있다.
+
+    생성자를 publlic하게 제공할 때, 인스턴스의 생성을 컨트롤할 수 없다. 즉, 어디서든 이 생성자를 호출해 새 인스턴스를 만들 수 있다.
+    사용하는 측에서 객체 생성을 얼마든지 원하는대로 생성할 수 있다.
+
+    만약 단 한 개의 인스턴스만 만들어야 한다면, 생성자가 아닌 정적 팩토리를 사용하여 인스턴스를 통제할 수 있다.
+    객체 생성을 팩토리 내에서 관리할 수 있으며, 생성 로직 컨트롤 또한 그 안에서 가능하다.
+    */
+
+    // private 접근 제어자 => 생성자를 호출 불가능
     private Settings() {}
 
+    // 인스턴스를 미리 생성
     private static final Settings SETTINGS = new Settings();
 
+    //미리 만들어놓은 인스턴스를 정적 팩토리로 제공
     public static Settings getInstance() {
         return SETTINGS;
     }

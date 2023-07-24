1653d33e52c707d90fa310983b81b7d9732c9d07
u-nij <u.nij0509@gmail.com>
2023-07-24 16:03:42 +0900

    아이템 2. 핵심 정리 2 - 빌더

--- src/main/java/me/whiteship/chapter01/item02/builder/BuilderTest.java
+++ src/main/java/me/whiteship/chapter01/item02/builder/BuilderTest.java
@@ -3,7 +3,24 @@ package me.whiteship.chapter01.item02.builder;
 public class BuilderTest {
 
     public static void main(String[] args) {
-        new NutritionFacts.Builder(10, 10)
+        new NutritionFacts.NutritionFactsBuilder()
+                .servingSize(10)
+                .calories(10)
+                .build();
+
+        /*
+        @Builder를 사용하면 필수값과 옵셔널한 값을 따로 받기 어려워진다.
+        필수적인 값을 생성자로 받고, 옵셔널한 값을 따로 받는 기능을 지원하지 않는다.
+
+        대안;
+        Builder(int servingSize, int servings) -> 기존의 Builder를 만들 때 생성자에 필수 필드를 생성하게 한 후,
+        NutritionFacts build() -> 옵셔널한 값을 메서드 체이닝으로 받는 방법.
+        new NutritionFacts.Builder(10, 10) // 필수 값
+                // 옵셔널 값
+                .calories(10)
+                .build();
+        */
+        new NutritionFacts.NutritionFactsBuilder() // Builder()의 이름이 NutritionFactsBuilder()가 되었다.
                 .calories(10)
                 .build();
     }
--- src/main/java/me/whiteship/chapter01/item02/builder/NutritionFacts.java
+++ src/main/java/me/whiteship/chapter01/item02/builder/NutritionFacts.java
@@ -1,6 +1,10 @@
 package me.whiteship.chapter01.item02.builder;
 
+import lombok.*;
+
 // 코드 2-3 빌더 패턴 - 점층적 생성자 패턴과 자바빈즈 패턴의 장점만 취했다. (17~18쪽)
+@Builder //(builderClassName = "Builder")
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class NutritionFacts {
     private final int servingSize;
     private final int servings;
@@ -9,11 +13,22 @@ public class NutritionFacts {
     private final int sodium;
     private final int carbohydrate;
 
+    /*
+    빌더 패턴
+    필수 속성에 해당하는 것들을 설정할 수 있어, 자바빈즈보다 객체를 안전하고 consistency하게 만들 수 있다.
+    But, 오히려 코드를 이해하기 어렵게 만든다. 필드가 중복된다.
+
+    모든 경우에 빌더 패턴을 만들지 말 것.
+    옵셔널한 매개변수가 많을 경우, immutable하게 만들고 싶다 => 빌더 패턴
+    */
+    /*
     public static void main(String[] args) {
+
         NutritionFacts cocaCola = new Builder(240, 8)
                 .calories(100)
                 .sodium(35)
-                .carbohydrate(27).build();
+                .carbohydrate(27) // Builder 리턴
+                .build(); // NutritionFacts 리턴
     }
 
     public static class Builder {
@@ -27,11 +42,14 @@ public class NutritionFacts {
         private int sodium        = 0;
         private int carbohydrate  = 0;
 
+        // 필수로 받아야하는 값 설정
         public Builder(int servingSize, int servings) {
             this.servingSize = servingSize;
             this.servings    = servings;
         }
 
+        // Setter와 비슷한 역할을 하는 것.
+        // return이 void가 아니라, Builder 타입 그 자체. => 플루언트 API 또는 메서드 체이닝이 가능해진다.
         public Builder calories(int val)
         { calories = val;      return this; }
         public Builder fat(int val)
@@ -53,5 +71,31 @@ public class NutritionFacts {
         fat          = builder.fat;
         sodium       = builder.sodium;
         carbohydrate = builder.carbohydrate;
+    }
+    */
+
+    /*
+    @Builder 어노테이션 작성시 사용 가능
+    
+    자바의 '어노테이션 프로세서'는 컴파일하는 시점에 어노테이션을 읽어들여 클래스를 만들고 코드를 조작할 수 있는 기능을 제공해준다.
+    @Builder 어노테이션을 통해 롬복이 제공하는 어노테이션 프로세서가 작성하지 않았던 코드(생성자)를 생성해주기 때문에,
+    위의 코드를 작성하지 않아도 된다.
+    (target/classes/me/whiteship/chater01/item02/builder/NutritionFacts 에서 확인 가능)
+
+    장점: 코드가 간결해진다.
+
+    단점1: 어노테이션을 통해 모든 파라미터를 받는 생성자가 기본적으로 생긴다.
+    -> @AllArgsConstructor(access = AcessLevel.PRIVATE)를 통해 내부에서만 사용 가능하게 할 수 있다.
+
+    단점2: 필수 값을 설정해줄 수 없다.
+
+    단점3: Builder()의 이름이 Builder가 아니게 된다.
+    -> @Builder 인자 값 builderClassName을 Builder로 설정해주면 된다.
+    */
+    public static void main(String[] args) {
+        NutritionFacts nutritionFacts = new NutritionFactsBuilder()
+                .servingSize(100)
+                .servings(10)
+                .build();
     }
 }

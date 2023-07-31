58d9a6460488765e38da865d736e60eca268c405
u-nij <u.nij0509@gmail.com>
2023-07-31 17:01:11 +0900

    아이템 3. 완벽 공략 13 - 객체 직렬화

--- src/main/java/me/whiteship/chapter01/item03/serialization/Book.java
+++ src/main/java/me/whiteship/chapter01/item03/serialization/Book.java
@@ -3,22 +3,38 @@ package me.whiteship.chapter01.item03.serialization;
 import java.io.Serializable;
 import java.time.LocalDate;
 
-public class Book implements Serializable {
+public class Book implements Serializable { // 인터페이스를 구현하지 않으면 NotSerializableException 예외 발생
 
+    /*
+    직렬화하는 시스템과 역직렬화하는 시스템이 다를 때, 직렬화 이후 클래스를 변경하게 되면
+    serialVersionUID가 일치하지 않아 InvalidClassException 예외가 발생한다.
+
+    임의로 serialVersionUID를 명시하여 역직렬화가 가능하도록 만들어줄 수 있다.
+    */
     private static final long serialVersionUID = 1L;
+
+    // static 필드의 값은 클래스에 할당되는 값이며, 인스턴스에 할당되는 값이 아니기 때문에 직렳화되지 않는다.
+    // 직렬화 후 클래스가 바뀌면 역직렬화가 될 것인가?
+    public static String staticField;
 
     private String isbn;
 
     private String title;
 
+    // 직렬화 후, author 코드 추가
+    private String author;
+
     private LocalDate published;
 
     private String name;
 
-    private transient int numberOfSold;
+    private transient int numberOfSold; // 직렬화를 하고 싶지 않은 데이터의 경우 transient 키워드 추가
 
-    public Book(String isbn, String title, String author, LocalDate published) {
+    public Book(String isbn, String title,
+                String author, // 직렬화 후, author 코드 추가
+                LocalDate published) {
         this.isbn = isbn;
+        this.author = author; // 직렬화 후, author 코드 추가
         this.title = title;
         this.published = published;
     }
@@ -29,7 +45,9 @@ public class Book implements Serializable {
                 "isbn='" + isbn + '\'' +
                 ", title='" + title + '\'' +
                 ", published=" + published +
+                ", author=" + author + // 직렬화 후, author 코드 추가
                 ", numberOfSold=" + numberOfSold +
+                ", staticField=" + staticField +
                 '}';
     }
 
--- src/main/java/me/whiteship/chapter01/item03/serialization/SerializationExample.java
+++ src/main/java/me/whiteship/chapter01/item03/serialization/SerializationExample.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 
 public class SerializationExample {
 
+    // 직렬화
     private void serialize(Book book) {
         try (ObjectOutput out = new ObjectOutputStream(new FileOutputStream("book.obj"))) {
             out.writeObject(book);
@@ -13,6 +14,7 @@ public class SerializationExample {
         }
     }
 
+    // 역직렬화
     private Book deserialize() {
         try (ObjectInput in = new ObjectInputStream(new FileInputStream("book.obj"))) {
             return (Book) in.readObject();
@@ -21,16 +23,45 @@ public class SerializationExample {
         }
     }
 
-    public static void main(String[] args) {
-//        Book book = new Book("12345", "이팩티브 자바 완벽 공략", "백기선",
+    /*
+    직렬화 및 역직렬화
+    */
+//    public static void main(String[] args) {
+//        Book book = new Book("12345", "이팩티브 자바 완벽 공략",
 //                LocalDate.of(2022, 3, 21));
-//        book.setNumberOfSold(200);
-
-        SerializationExample example = new SerializationExample();
+//        book.setNumberOfSold(200); // 직렬화할 떄 200, 역직렬화할 때 0(직렬화하지 않았기 때문)
+//
+//        SerializationExample example = new SerializationExample();
+//        Book.staticField = "effective";
 //        example.serialize(book);
-        Book deserializedBook = example.deserialize();
-
+//        Book.staticField = "java"; // static 필드 값은 직렬화가 되지 않기 때문에 역직렬화할 때에도 "java"가 프린트 된다.
+//        Book deserializedBook = example.deserialize();
+//
 //        System.out.println(book);
+//        System.out.println(deserializedBook);
+//    }
+
+    /*
+    직렬화
+    */
+//    public static void main(String[] args) {
+//        Book book = new Book("12345", "이팩티브 자바 완벽 공략",
+//                LocalDate.of(2022, 3, 21));
+//        book.setNumberOfSold(200); // 직렬화할 떄 200, 역직렬화할 때 0(직렬화하지 않았기 때문)
+//
+//        SerializationExample example = new SerializationExample();
+//
+//        example.serialize(book);
+//        System.out.println(book);
+//    }
+
+    /*
+    역직렬화
+    */
+    public static void main(String[] args) {
+        SerializationExample example = new SerializationExample();
+
+        Book deserializedBook = example.deserialize();
         System.out.println(deserializedBook);
     }
 }

96db3423d21cb65d1fbad64e2c451244e01d3ce3
u-nij <u.nij0509@gmail.com>
2023-08-03 17:15:51 +0900

    아이템 5. 완벽 공략 15 - 스프링 IoC

--- src/main/java/me/whiteship/chapter01/item05/springioc/App.java
+++ src/main/java/me/whiteship/chapter01/item05/springioc/App.java
@@ -6,8 +6,26 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class App {
 
     public static void main(String[] args) {
+        /*
+        // Spring이 모르는 객체
+        SpellChecker spellChecker1 = new SpellChecker();
+         */
+
+        /*
+        Spring을 사용해 가져온 SpellCheck는 Spring Bean.
+        Spring IoC Container인 ApplicationContext에서 가져왔고,
+        그 ApplicationContext는 우리가 제공한 설정 파일을 통해 Bean들을 만들었기 때문에
+        이 SpellCheck는 Spring이 만들어준 것.
+
+        별도의 설정을 하지 않았기 때문에 싱글톤으로 만들어졌으며,
+        스프링이 제공하는 라이프사이클 인터페이스를 거친다.
+
+        SpellChecker와 SpringDictionary 어디에도 스프링과 관련된 코드는 들어가지 않는다.
+        POJO를 유지한 상태에서, 외부인 App 클래스에서 인스턴스를 만들고 의존성을 주입해 꺼내 사용하기만 하면 된다.
+
+         */
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         SpellChecker spellChecker = applicationContext.getBean(SpellChecker.class);
-        spellChecker.isValid("test");
+        spellChecker.isValid("test"); // contains test 출력 => 주입된 SpringDictionary가 사용되어 contains() 메서드가 호출되었기 때문.
     }
 }
--- src/main/java/me/whiteship/chapter01/item05/springioc/AppConfig.java
+++ src/main/java/me/whiteship/chapter01/item05/springioc/AppConfig.java
@@ -1,10 +1,40 @@
 package me.whiteship.chapter01.item05.springioc;
 
+import me.whiteship.chapter01.item05.Dictionary;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+/*
+스프링이 제공하는 Configuration 어노테이션을 이용해, 스프링을 사용하기 위한 설정 파일 정의.
+자바의 클래스 파일이지만, 스프링에게는 설정 파일.
+ */
+@Configuration // 스프링에게 필요한 설정 파일. 자바 설정 파일임을 명시.
+/*
+일일히 Bean을 정의하기 번거롭기 때문에 @ComponentScan을 사용.
+인스턴스를 스프링이 관리하게 하려면 @Component을 붙여주고, 스프링 설정 파일에서 @ComponentScan을 사용한다.
+
+@Component; Bean이 될 클래스와 Bean이 아닐 클래스를 구분하기 위한 마킹.
+basePackageClasses; 해당 클래스가 위치한 곳에 있는 패키지부터 @Component 탐색.
+ */
 @ComponentScan(basePackageClasses = AppConfig.class)
 public class AppConfig {
 
+    /*
+    @ComponentScan을 사용하지 않을 때
+
+    SpellChecker는 이 Bean 설정 파일에 의해 스프링이 만들어주게 된다.
+    스프링이 만들어준 SpellChecker를 꺼내야 스프링이 의존성 주입해준 SpellChecker를 꺼낼 수 있다.
+    외부에서 new를 통해 SpellChecker를 생성하게 되면, 스프링은 그 SpellChecker를 모르게 된다.
+
+    @Bean
+    public SpellChecker spellChecker(Dictionary dictionary) { // 스프링이 관리하는 Dictionary를 넘겨줌.
+        return new SpellChecker(dictionary);
+    }
+
+    @Bean
+    public Dictionary dictionary() {
+        return new SpringDictionary();
+    }
+     */
 }

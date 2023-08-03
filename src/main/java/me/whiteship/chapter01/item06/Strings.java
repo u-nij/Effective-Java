dbfd731b7218c5a0d9f2dbde4005d1ef2fb8500c
u-nij <u.nij0509@gmail.com>
2023-08-03 18:02:37 +0900

    아이템 6. 핵심 정리

--- src/main/java/me/whiteship/chapter01/item06/Strings.java
+++ src/main/java/me/whiteship/chapter01/item06/Strings.java
@@ -3,16 +3,23 @@ package me.whiteship.chapter01.item06;
 public class Strings {
 
     public static void main(String[] args) {
+        /*
+        JVM은 내부적으로 문자열을 모두 Constant Pool에서 캐싱하고 있다고 생각하면 된다.
+        일단 생성된 문자열을 담아두고, 동일한 문자열을 사용할 경우 풀에서 참조하는 방법을 사용한다.
+
+        문자열을 비교할 때는 eqauls를 사용하여 값 자체가 같은지 비교한다.
+        문자열 값이 같더라도 인스턴스가 다른 경우를 대비하기 위해 항상 eqauls를 통해 비교하라고 한다.
+         */
         String hello = "hello";
 
         //TODO 이 방법은 권장하지 않습니다.
-        String hello2 = new String("hello");
+        String hello2 = new String("hello"); // 불필요하게 객체를 만듦.
 
         String hello3 = "hello";
 
-        System.out.println(hello == hello2);
-        System.out.println(hello.equals(hello2));
-        System.out.println(hello == hello3);
-        System.out.println(hello.equals(hello3));
+        System.out.println(hello == hello2); // false
+        System.out.println(hello.equals(hello2)); // true
+        System.out.println(hello == hello3); // true
+        System.out.println(hello.equals(hello3)); // true
     }
 }
--- src/main/java/me/whiteship/chapter01/item06/Sum.java
+++ src/main/java/me/whiteship/chapter01/item06/Sum.java
@@ -5,10 +5,19 @@ public class Sum {
         // TODO Long을 long으로 변경하여 실행해 보세요.
         Long sum = 0L;
         for (long i = 0; i <= Integer.MAX_VALUE; i++)
-            sum += i;
+            sum += i; // Long + long 이기 때문에, 객체 i의 타입이 Integer.MAX_VALUE 개수만큼 Long으로 바뀜.
         return sum;
     }
 
+
+
+    /*
+    primitive 타입을 wrapper 타입으로 변형하는 과정을 AutoBoxing.
+    wrapper 타입을 primitive 타입으로 변형하는 과정을 UnBoxing이라고 한다.
+    해당 과정을 JVM의 자바 컴파일러가 런타임에 자동으로 처리해준다.
+
+    불필요한 AutoBoxing으로 불필요하게 많은 객체를 만들게 되는 것을 조심해야 한다.
+     */
     public static void main(String[] args) {
         long start = System.nanoTime();
         long x = sum();

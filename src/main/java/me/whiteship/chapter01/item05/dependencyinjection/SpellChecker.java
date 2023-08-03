76e6603a2a4bbd240f3d9b0a49d02d1cafe3fa96
u-nij <u.nij0509@gmail.com>
2023-08-03 15:29:18 +0900

    아이템 5. 완벽 공략

--- src/main/java/me/whiteship/chapter01/item05/dependencyinjection/SpellChecker.java
+++ src/main/java/me/whiteship/chapter01/item05/dependencyinjection/SpellChecker.java
@@ -1,5 +1,6 @@
 package me.whiteship.chapter01.item05.dependencyinjection;
 
+import me.whiteship.chapter01.item05.DefaultDictionary;
 import me.whiteship.chapter01.item05.Dictionary;
 
 import java.util.List;
@@ -28,9 +29,51 @@ public class SpellChecker {
         this.dictionary = dictionary;
     }
 
+    /*
+    자원 팩터리를 넘겨주는 방식
+    public SpellChecker(DictionaryFactory dictionaryFactory) {
+        this.dictionary = DictionaryFactory.get();
+    }
+    */
+
+    /*
+    매개변수 없이 무언가 가져오기만 하는 것이라면 팩터리를 만들 필요 없이, Supplier 인터페이스를 사용할 수 있다.
+     */
     public SpellChecker(Supplier<Dictionary> dictionarySupplier) {
         this.dictionary = dictionarySupplier.get();
     }
+
+    /*
+    "한정적 와일드카드 타입을 사용해 팩터리의 타입 매개변수를 제한해야 한다."
+
+    <? extends Dictionary>와 같은 선언을 통해,
+    즉 제네릭 타입으로 Dictionary 타입에 호환하는 하위 타입 또한 가질 수 있다.
+    Dictionary 타입만 받을 수 있는지, 해당 타입에 호환되는 하위 타입까지 받을 수 있는지 한정적 와일드 카드를 설정하라는 것.
+    public SpellChecker(Supplier<? extends Dictionary> dictionarySupplier) {
+        this.dictionary = dictionarySupplier.get();
+    }
+
+    타입을 한정하지 않았을 때도, 이미 Dictionary 타입을 리턴하는 모든 팩토리에는 맞게 된다.
+    구체적인 타입의 Supplier에서 시작된 말이 아닐까 싶음. (ex. Supplier<DefaultDictionary>)
+
+    Supplier<DefaultDictionary>  vs. Supplier<? extends Dictionary>
+    전자와 같이 구체적으로 타입이 정해져 있다면, 다른 타입의 Dictionary를 만드는 것은 주입을 하지 못한다. Dictionary 타입만 받을 수 있다.
+    후자와 같이 타입 한정자를 사용하게 되면, 하위 타입까지 더 폭넓게 받을 수 있기 때문.
+
+    하지만, Dictionary 타입을 리턴하고 인터페이스를 구현했다면 Supplier<Dictionary>로도(즉, 타입 한정자를 사용하지 않아도) 충분하다.
+
+
+    아마도 또다른 타입의 Dictionary 타입이 있다고 가정했을 때,
+    SpeellCheckerTest의 SpellChecker 메서드가 "Supplier<DefaultDictionary>"를 통해 구체적인 타입의 Supplier를 받는게 아니라
+    좀더 폭넓은 타입을 받기 위해 "Supplier<? extends Dictionary>"라고 설정했을 것이라 추축.
+    public SpellChecker(Supplier<DefaultDictionary> dictionarySupplier) {
+        this.dictionary = dictionarySupplier.get();
+    }
+    일 때, SpellCheckerTest의 "SpellChecker spellChecker = new SpellChecker(MockDictionary::new);" 코드가 에러가 난다.
+
+    하지만 굳이, Supplier<? extends Dictionary> 라고 작성하지 않아도
+    Dictionary 타입이고 인터페이스를 구현했다면 Supplier<Dictionary> 라고 작성해도 Dictionary 하위 타입에 호환되기만 하면 주입해줄 수 있다.
+     */
 
     public boolean isValid(String word) {
         // TODO 여기 SpellChecker 코드
--- src/test/java/me/whiteship/chapter01/item05/dependencyinjection/SpellCheckerTest.java
+++ src/test/java/me/whiteship/chapter01/item05/dependencyinjection/SpellCheckerTest.java
@@ -1,8 +1,11 @@
 package me.whiteship.chapter01.item05.dependencyinjection;
 
 import me.whiteship.chapter01.item05.DefaultDictionary;
+import me.whiteship.chapter01.item05.Dictionary;
 import me.whiteship.chapter01.item05.MockDictionary;
 import org.junit.jupiter.api.Test;
+
+import java.util.function.Supplier;
 
 class SpellCheckerTest {
 
@@ -11,10 +14,14 @@ class SpellCheckerTest {
         /*
         의존 객체 주입을 사용한 코드.
         SpellChecker 코드 '외부'에서 해당 클래스가 사용하는 리소스, 즉 의존 객체를 주입해줄 수 있다.
-
         SpellChecker의 코드를 그대로 사용하면서, 테스트용으로 사용하고자 하는 다른 Dictionary로 얼마든지 교체가 가능하다.
+
+        메서드 레퍼런스의 타겟 타입이 SpellChecker(Supplier<Dictionary> dictionarySupplier)에 준하는 코드가 된다.
+        (dictionarySupplier를 누르면 해당 생성자로 이동하는 것을 확인 가능)
          */
-        SpellChecker spellChecker = new SpellChecker(MockDictionary::new);
+        Supplier<Dictionary> mockDictionary = MockDictionary::new;
+        SpellChecker spellChecker = new SpellChecker(mockDictionary); // 타겟 타입이 Supplier 인터페이스인 메서드 참조
+        SpellChecker spellChecker2 = new SpellChecker(DefaultDictionary::new);
         spellChecker.isValid("test");
     }
 

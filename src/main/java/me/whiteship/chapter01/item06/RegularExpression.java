99f315b44c80594c592ac07b7b1815270286ad53
u-nij <u.nij0509@gmail.com>
2023-08-04 10:59:51 +0900

    아이템 6. 완벽 공략 17 - 정규 표현식

--- src/main/java/me/whiteship/chapter01/item06/RegularExpression.java
+++ src/main/java/me/whiteship/chapter01/item06/RegularExpression.java
@@ -10,8 +10,14 @@ public class RegularExpression {
         long start = System.nanoTime();
         for (int j = 0; j < 10000; j++) {
             String name = "keesun,whiteship";
-            name.split(",");
-//            SPLIT_PATTERN.split(name);
+            /*
+            한 글자의 구분자를 이용해 문자열을 분리할 때,
+            split()을 사용하는 것이 미리 컴파일한 패턴을 호출하는 것보다 더 빠르다.
+
+            여러 글자의 경우, 패턴을 미리 컴파일해 재사용하는 것이 유리하다.
+             */
+            name.split(","); // 컴파일하지 않았을 때
+//            SPLIT_PATTERN.split(name); // 미리 컴파일한 패턴을 호출하는 것.
         }
         System.out.println(System.nanoTime() - start);
     }

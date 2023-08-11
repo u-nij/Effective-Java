9bcdd4b163bd5e6eabc3f4a32243eb2ed85815b5
u-nij <u.nij0509@gmail.com>
2023-08-11 09:34:13 +0900

    아이템 8. 완벽 공략 23 - AutoClosable

--- src/main/java/me/whiteship/chapter01/item08/autoclosable/App.java
+++ src/main/java/me/whiteship/chapter01/item08/autoclosable/App.java
@@ -1,5 +1,7 @@
 package me.whiteship.chapter01.item08.autoclosable;
 
+import java.io.IOException;
+
 public class App {
 
     public static void main(String[] args) {
@@ -15,9 +17,23 @@ public class App {
          */
 
         // try-with-resource 문법
-        try(AutoClosableIsGood good = new AutoClosableIsGood("")) {
+        /*
+        AutoCloseable 인터페이스를 사용하면 try-with-resource 문법에 사용할 수 있다.
+        자원 반납을 자동으로 해주기 떄문에 finally 블럭을 쓰지 않아도 된다.
+        finally 블럭을 작성하지 않았거나, 잘못 작성했을 경우 자원 반납이 제대로 되지 않는 문제를 방지할 수 있다.
+         */
+        try(AutoClosableIsGood good = new AutoClosableIsGood(""); // 사용할 리소르를 정의.
+            AutoClosableIsGood good1 = new AutoClosableIsGood("")) { // ';'를 구분자로 여러 개 정의할 수 있다.
             // TODO 자원 반납 처리가 됨.
 
+            good.close(); // Closeable; 명시적으로 호출할 수도 있기 때문에 반드시 idempotent 해야 한다.
         }
+        /*
+        // 1. 그냥 던질 경우 클라이언트 측이 IOException 에러를 잡아줘야 한다.
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+         */
+        // 2,3. close 메서드에서 예외를 처리했기 때문에, 클라이언트 측에서 예외를 처리하지 않아도 된다. catch 구문 삭제
     }
 }
